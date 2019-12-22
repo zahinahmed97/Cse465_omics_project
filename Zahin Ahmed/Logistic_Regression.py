5b7bdc72ccb4658cbd4ebffa7e56576c8c575a17
zahinahmed97 <zahinahmed97@gmail.com>
2019-12-23 02:19:53 +0600

    added ROC AUC curve to LR

--- Zahin Ahmed/Logistic_Regression.py
+++ Zahin Ahmed/Logistic_Regression.py
@@ -9,9 +9,9 @@ import numpy as np
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import precision_recall_fscore_support
 from sklearn.utils.multiclass import unique_labels
 from sklearn.metrics import confusion_matrix
+from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import scale
 
@@ -35,7 +35,7 @@ y = y.as_matrix()
 
 X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=42)
 print("Performing Logistic Regression")
-logreg= LogisticRegression(penalty='l2',solver='lbfgs', max_iter=500, C=4)
+logreg= LogisticRegression(penalty='l2',solver='lbfgs', max_iter=500, C=1)
 logreg.fit(X_train,y_train)
 
 y_pred=logreg.predict(X_test)
@@ -43,11 +43,38 @@ conf=confusion_matrix(y_test,y_pred , sample_weight=None)
 labels = unique_labels(y_test, y_pred)
 res_conf=conf.ravel().tolist()
 
-
+print (res_conf)
 
 report=pd.DataFrame(res_conf, index = ['TN','FP','FN','TP'])
 report.to_csv("C:\\Users\\Zahin Ahmed\\Desktop\\CSE465_omics_project\\Zahin Ahmed\\report_LR", sep='\t')
 
+logreg.predict(X_test)[0:10]
+logreg.predict_proba(X_test)[0:10, :]
+logreg.predict_proba(X_test)[0:10,1]
+y_pred_prob= logreg.predict_proba(X_test)[:,1]
+import matplotlib.pyplot as plt
+
+plt.hist(y_pred_prob, bins=0)
+plt.xlim(0,1)
+plt.title('Histogram of predicted probabilities')
+plt.xlabel('predicted probability of resistance')
+plt.ylabel('frequency')
+
+from sklearn.preprocessing import binarize
+y_pred_class= binarize([y_pred_prob], 0.3)[0]
+
+print(confusion_matrix(y_test, y_pred_class))
+
+fpr, tpr, thresholds= metrics.roc_curve(y_test, y_pred_prob)
+plt.plot(fpr,tpr)
+plt.xlim([0.0,1.0])
+plt.ylim([0.0,1.0])
+plt.title('ROC curve for AMX resistance')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.grid(True)
+
+print (metrics.roc_auc_score(y_test,y_pred_prob))
 print(logreg)
 print(report)
 print("Done!")
